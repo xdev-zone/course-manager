388fb3387112d0ceda0f28a6d5b20f892fe7bee3
xdev-zone <69912658+xdev-zone@users.noreply.github.com>
2022-08-22 00:29:59 +0200

    div

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://serveradmin:course.manager1@course-manager-db1.mysql.database.azure.com/coursemanager'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
